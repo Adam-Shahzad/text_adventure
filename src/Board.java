5d820518af13dbcb13eee26cde524914a3413236
Profile <Profile>
2017-07-28 11:02:20 +0100

    extra obstacles placed

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Board {
 public static void main(String[] args){
+	
 	Board b = new Board();
 	Player p = new Player(0, 0, null);
 	goal g = new goal();
-	obsticle o = new obsticle();
+	
+	obsticle o1 = new obsticle(1,2);
+	obsticle o2 = new obsticle(3,1);
+	obsticle o3 = new obsticle(0,-1);
+	
+	ArrayList<obsticle> oList = new ArrayList();
+	
+	oList.add(o1);
+	oList.add(o2);
+	oList.add(o3);
+	
 	
 	b.start();
 	
@@ -18,7 +30,7 @@ public static void main(String[] args){
 	
 		b.goal_status(g, p);
 		
-		b.hit_status(o, p);
+		b.hit_status(oList, p);
 		
 	}
 	
@@ -37,6 +49,7 @@ public void start()
 {
 	
 	System.out.println("You Are in an infinite forest look for the goal and avoid the obstacles");
+	System.out.println("remember you only have 3 lives");
 }
 
 public void show_compass(Player p, goal g){
@@ -50,11 +63,13 @@ public void goal_status(goal g, Player p){
 	
 }
 
-public void hit_status(obsticle o, Player p){
-	
-	String is_hit=p.check_obstacle(o, p);
+public void hit_status(ArrayList<obsticle> oList, Player p){
+	for(int i =0; i<oList.size(); i++){
+		
+	String is_hit=p.check_obstacle(oList.get(i), p);
 	if (!is_hit.equals("empty")){
 		System.out.println(is_hit);
+	}
 	}
 }
 }
--- src/obsticle.java
+++ src/obsticle.java
@@ -1,7 +1,13 @@
 
 public class obsticle extends Place {
-	int x_value = 1;
-	int y_value = 2;
+	int x_value;
+	int y_value;
+
+	public obsticle(int x_value, int y_value) {
+		super();
+		this.x_value = x_value;
+		this.y_value = y_value;
+	}
 
 	public String return_text(Player p) {
 		return "you hit an obstacle \nYou have " +p.getHit_points()+ " lives left";
